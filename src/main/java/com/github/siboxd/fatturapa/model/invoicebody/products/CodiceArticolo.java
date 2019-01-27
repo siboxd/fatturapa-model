2ed3fc6ff5f98e4068f3d1c7967342d6c28fbc2c
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-01-27 17:12:40 +0100

    Refactored "CodiceArticolo" class

--- src/main/java/com/github/siboxd/fatturapa/model/invoicebody/products/CodiceArticolo.java
+++ src/main/java/com/github/siboxd/fatturapa/model/invoicebody/products/CodiceArticolo.java
@@ -1,5 +1,6 @@
 package com.github.siboxd.fatturapa.model.invoicebody.products;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
@@ -15,42 +16,38 @@ import org.simpleframework.xml.Root;
 public final class CodiceArticolo {
 
     @Element(name = "CodiceTipo")
-    private String codiceTipo;
+    private final String codiceTipo;
 
     @Element(name = "CodiceValore")
-    private String codiceValore;
+    private final String codiceValore;
 
-    public CodiceArticolo() {
+    /**
+     * @param codiceTipo   It is used to identify the coding standard of the product.
+     *                     <p>
+     *                     The field must contain an indication of the type of coding of the item
+     *                     expressed in the CodeValue field (eg "EAN", "SSC", "TARIC", "CPV", or
+     *                     "Supplier Code", "Code Art. customer ".......).
+     * @param codiceValore It serves to give the possibility of unique identification of a product in relation to
+     *                     the type of code.
+     *                     <p>
+     *                     The field must contain the indication of the value of the article
+     *                     code corresponding to the type of coding of the article.
+     */
+    public CodiceArticolo(@Element(name = "CodiceTipo") @NonNull final String codiceTipo,
+                          @Element(name = "CodiceValore") @NonNull final String codiceValore) {
+
+        this.codiceTipo = codiceTipo;
+        this.codiceValore = codiceValore;
     }
 
+    @NonNull
     public String getCodiceTipo() {
         return codiceTipo;
     }
 
-    /**
-     * It is used to identify the coding standard of the product.
-     *
-     * @param codiceTipo The field must contain an indication of the type of coding of the item
-     *                   expressed in the CodeValue field (eg "EAN", "SSC", "TARIC", "CPV", or
-     *                   "Supplier Code", "Code Art. customer ".......).
-     */
-    public void setCodiceTipo(final String codiceTipo) {
-        this.codiceTipo = codiceTipo;
-    }
-
+    @NonNull
     public String getCodiceValore() {
         return codiceValore;
-    }
-
-    /**
-     * It serves to give the possibility of unique identification of a product in relation to
-     * the type of code.
-     *
-     * @param codiceValore The field must contain the indication of the value of the article
-     *                     code corresponding to the type of coding of the article.
-     */
-    public void setCodiceValore(final String codiceValore) {
-        this.codiceValore = codiceValore;
     }
 
 }
