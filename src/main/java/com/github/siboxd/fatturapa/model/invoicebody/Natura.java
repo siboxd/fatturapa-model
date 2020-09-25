49a9bc75e9fa8ecbd017dd8269d99bcfb3874f25
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2020-09-25 12:02:48 +0200

    Implemented changes in Natura

--- src/main/java/com/github/siboxd/fatturapa/model/invoicebody/Natura.java
+++ src/main/java/com/github/siboxd/fatturapa/model/invoicebody/Natura.java
@@ -15,11 +15,17 @@ public enum Natura {
     N1,
     /**
      * Operations Not subject
+     *
+     * <b>Will not be valid after 01/01/2021. Use the sub-codes</b>
      */
+    @Deprecated
     N2,
     /**
      * Non-Taxable Operations
+     *
+     * <b>Will not be valid after 01/01/2021. Use the sub-codes</b>
      */
+    @Deprecated
     N3,
     /**
      * Exempt Operations
@@ -31,7 +37,10 @@ public enum Natura {
     N5,
     /**
      * Reverse charge
+     *
+     * <b>Will not be valid after 01/01/2021. Use the sub-codes</b>
      */
+    @Deprecated
     N6,
     /**
      * VAT paid in other EU countries (distance sales pursuant to Article 40 paragraphs 3 and 4
@@ -39,6 +48,86 @@ public enum Natura {
      * tele-broadcasting and electronic services pursuant to Article 7-sexies letter f, g,
      * presidential decree 633/72 and article 74 sexies, presidential decree 633/72)
      */
-    N7
+    N7,
 
+
+    // v1.2.1
+
+    /**
+     * Not subject to VAT pursuant to art. 7 to 7-septies of Presidential Decree 633/72
+     */
+    N2_1,
+    /**
+     * Not subject - other cases
+     */
+    N2_2,
+
+    /**
+     * Non-taxable - exports
+     */
+    N3_1,
+    /**
+     * Non-taxable - intra-community transfers
+     */
+    N3_2,
+    /**
+     * Non-taxable - sales to San Marino
+     */
+    N3_3,
+    /**
+     * Non Taxable - transactions similar to export sales
+     */
+    N3_4,
+    /**
+     * Non Taxable - following declarations of intent
+     */
+    N3_5,
+    /**
+     * Non-taxable - other transactions that do not contribute to the formation of the ceiling
+     */
+    N3_6,
+
+    /**
+     * Reverse charge - disposal of scrap and other recycled materials
+     */
+    N6_1,
+    /**
+     * Reverse charge - sale of pure gold and silver
+     */
+    N6_2,
+    /**
+     * Reverse charge - subcontracting in the construction sector
+     */
+    N6_3,
+    /**
+     * Reverse charge - sale of buildings
+     */
+    N6_4,
+    /**
+     * Reverse charge - sale of mobile phones
+     */
+    N6_5,
+    /**
+     * Reverse charge - disposal of electronic products
+     */
+    N6_6,
+    /**
+     * Reverse charge - construction sector performance and related sectors
+     */
+    N6_7,
+    /**
+     * Reverse charge - energy sector operations
+     */
+    N6_8,
+    /**
+     * Reverse charge - other cases
+     */
+    N6_9;
+
+    @Override
+    public String toString() {
+        final String s = super.toString();
+        if (s.contains("_")) return s.replace("_", ".");
+        else return s;
+    }
 }
